86e2167cc859f5465e22293972336e2e1433b30e
maxyxy123 <maxminh716@gmail.com>
2026-01-23 16:22:57 +0700

    Update wordScramble.js 4:22 pm Time and check answer

--- wordScramble/wordScramble.js
+++ wordScramble/wordScramble.js
@@ -1,55 +1,63 @@
-import { api } from "./api.js"
+import { api } from "./api.js";
 
-const displayWord = document.querySelector('.display')
-const hints = document.querySelector('.hint')
-const time = document.querySelector('.time')
-const input = document.getElementById('input')
-const categorys = document.querySelector('.category')
-const refresh = document.querySelector('.refresh')
-const check= document.querySelector('.check')
+const displayWord = document.querySelector(".display");
+const hints = document.querySelector(".hint");
+const time = document.querySelector(".time");
+const input = document.getElementById("input");
+const categorys = document.querySelector(".category");
+const refresh = document.querySelector(".refresh");
+const check = document.querySelector(".check");
 
+let timerId;
+let word = "";
+check.addEventListener("click", async () => {
+  const answer = input.value.toLowerCase();
+  console.log(answer);
+  console.log(word);
+  if (answer === word) {
+    alert("Correct");
+    renderWords();
+  }
+});
 
-let checkIndex = []
-let randomIndex = ''
-async function renderWords(){
-    const data = await api();
-    const word = data.word
-    const finalWord = word.split('')
-    
-    const a =[...finalWord];
-    for(let i = a.length -1 ; i >0 ; i--){
-        const j = Math.floor(Math.random() * ( i+1 ));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-     console.log(a)
-     let display = '';
-     for(let i = 0; i<a.length;i++){
-        display += a[i]+ '  '
-     }
-     displayWord.textContent = display;
+async function renderWords() {
+  if (timerId) {
+    clearInterval(timerId);
+  }
+  const data = await api();
+  word = data.word;
+  const finalWord = word.split("");
+  console.log(word);
+  const a = [...finalWord];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
 
-     categorys.textContent = `Category :${data?.category ?? 'No data'}`
-     hints.textContent = `Hint :${data?.hint ?? ' No data'}`
+  let display = "";
+  for (let i = 0; i < a.length; i++) {
+    display += a[i] + "  ";
+  }
+  displayWord.textContent = display;
 
+  categorys.textContent = `Category :${data?.category ?? "No data"}`;
+  hints.textContent = `Hint :${data?.hint ?? " No data"}`;
 
-   
-   
-   
+  countDown();
+
+  return word;
 }
 renderWords();
-countDown()
 
-function countDown(seccond = 30){
-   let remaining = seccond - 1 ;
-   let timerId;
-   timerId = setInterval(()=>{
+function countDown(seccond = 3) {
+  let remaining = "";
+
+  timerId = setInterval(() => {
+    remaining = seccond-- + "s";
     time.textContent = remaining;
-   },1000)
-   if(remaining < 0){
-    clearInterval(timerId);
-   }
+    if (remaining < 0) {
+      clearInterval(timerId);
+      renderWords();
+    }
+  }, 1000);
 }
-
-
-
-
